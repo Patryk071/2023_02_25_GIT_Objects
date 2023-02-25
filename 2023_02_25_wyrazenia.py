e33050b9939a8808fda35a35b1ded6406d9a2d31
Patryk_G <pw28358@student.wsb.wroclaw.pl>
2023-02-25 14:35:23 +0100

    Zajecia z dn. 25.02.2023 - commit 4, z wyrażeniami

--- 2023_02_25_wyrazenia.py
+++ 2023_02_25_wyrazenia.py
@@ -1,6 +1,7 @@
+#Lista z liczbami
 liczby = [1, 2, 3, 4, 5, 6, 7]
 
-#1 sposób
+#1 sposób - wybrać tylko elementy parzyste z listy
 liczbyParzyste1 = []
 
 for liczba in liczby:
@@ -9,7 +10,7 @@ for liczba in liczby:
 
 print(liczbyParzyste1)
 
-#sposób 2
+#sposób 2 - wybrać tylko elementy parzyste z listy
 liczbyParzyste2 = [element
                    for element in liczby
                    if (element % 2) == 0]
@@ -22,3 +23,42 @@ liczbyDoKwadratu = [element**2
                     for element in liczby]
 
 print(liczbyDoKwadratu)
+
+
+#WYRAŻENIA NA SŁOWNIKACH
+temperaturaC = {'temp8': -10,
+               'temp12': 0,
+               'temp16': -10,
+               'temp22': -26}
+
+print("Temperatura w Celcjuszach: ", temperaturaC)
+
+
+temperaturaF = {key: value * 1.8 + 32
+                for key, value in temperaturaC.items()
+                }
+
+print("Temperatura w Fahrenheitach: ", temperaturaF)
+
+
+#Generator list
+import sys
+
+parzyste3_bez_generatora = [element
+                            for element in range(8000)
+                            if (element % 2) == 0
+                            ]
+print(parzyste3_bez_generatora)
+print(sys.getsizeof(parzyste3_bez_generatora))
+
+
+parzyste3_z_generatorem = (element
+                            for element in range(1000000)
+                            if (element % 2) == 0
+                           )
+
+for item in parzyste3_z_generatorem:
+    print(item)
+
+print(parzyste3_z_generatorem)
+print(sys.getsizeof(parzyste3_z_generatorem))
